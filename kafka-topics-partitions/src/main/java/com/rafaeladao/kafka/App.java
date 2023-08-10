aec188981be4de46d477e26ffb8d9b7a0f64aba8
RafaelAdao <rafael.adao@hotmail.com.br>
2023-08-10 19:33:42 -0300

    init kafka-topics-partitions

---
+++ kafka-topics-partitions/src/main/java/com/rafaeladao/kafka/App.java
@@ -0,0 +1,13 @@
+package com.rafaeladao.kafka;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
